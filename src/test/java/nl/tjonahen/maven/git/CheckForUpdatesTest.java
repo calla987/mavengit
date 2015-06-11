44e12e89687c2ce701c05af1eaf4dbc8a21ceaf5
Philippe Tjon - A - Hen <philippe@tjonahen.nl>
2015-06-11 08:29:48 +0200

    Removed default app, added jGit sample (my own)

---
+++ src/test/java/nl/tjonahen/maven/git/CheckForUpdatesTest.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2015 Philippe Tjon - A - Hen, philippe@tjonahen.nl
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package nl.tjonahen.maven.git;
+
+import java.io.IOException;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.junit.Test;
+
+/**
+ *
+ * @author Philippe Tjon - A - Hen, philippe@tjonahen.nl
+ */
+public class CheckForUpdatesTest {
+    
+
+    @Test
+    public void testExecute() throws IOException, GitAPIException {
+        final CheckForUpdates sut = new CheckForUpdates();
+        
+        sut.execute("/home/ordina/tmp/mavengit");
+    }
+    
+}
